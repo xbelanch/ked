64f0840515d84993a337382208c13fb0953f6c47
Xavier Belanche <xbelanch@protonmail.com>
2021-12-27 19:44:23 +0100

    Show a pinky window

--- rogueban.c
+++ rogueban.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 void sdl_check_code(int code)
@@ -24,6 +25,26 @@ int main(int argc, char *argv[])
     (void) argc;
     (void) argv[0];
     sdl_check_code(SDL_Init(SDL_INIT_VIDEO));
+    SDL_Window *window = sdl_check_pointer(SDL_CreateWindow("Rogueban", 0, 0, 512, 512, SDL_WINDOW_RESIZABLE));
+    SDL_Renderer *renderer = sdl_check_pointer(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+
+    bool quit = false;
+
+    while (!quit) {
+        SDL_Event event = {0};
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT: {
+                quit = true;
+                break;
+            }
+            }
+
+            sdl_check_code(SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255));
+            sdl_check_code(SDL_RenderClear(renderer));
+            SDL_RenderPresent(renderer);
+        }
+    }
 
     SDL_Quit();
     return(0);
