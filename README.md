39df22536902b8aa63dbc29b9a4bccb763793835
Xavier Belanche <xbelanch@protonmail.com>
2021-12-28 20:57:15 +0100

    Our hero is alive at the top-left corner of the screen!

--- README.md
+++ README.md
@@ -6,4 +6,7 @@
 
 - [Code Page 437](https://cp437.github.io/)
 - [fontbuilder](https://github.com/andryblack/fontbuilder)
+- [STB Image](https://github.com/nothings/stb/blob/master/stb_image.h)
+- [Bitmap font to C Array converter](https://www.micro.in.th/esp32-font-converter/)
+- [Bitmap Font data as array in C](https://unix.stackexchange.com/questions/119236/bitmap-font-data-as-array-in-c/134167)
 - [Roguelike Vision Algorithms](http://www.adammil.net/blog/v125_roguelike_vision_algorithms.html#raycode)
--- rogueban.c
+++ rogueban.c
@@ -3,6 +3,11 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "./stb_image.h"
+
+#define FONT "./perfect_dos_font/perfect_dos_vga_437_regular_24.PNG"
+
 void sdl_check_code(int code)
 {
     if (code < 0) {
@@ -20,6 +25,40 @@ void *sdl_check_pointer(void *ptr)
     return ptr;
 }
 
+SDL_Surface *get_suface_from_file(const char *file_path)
+{
+    // Source code robbed from https://wiki.libsdl.org/SDL_CreateRGBSurfaceFrom
+    int req_format = STBI_rgb_alpha;
+    int width, height, orig_format;
+    unsigned char* pixels = stbi_load(file_path, &width, &height, &orig_format, req_format);
+    if(pixels == NULL) {
+        SDL_Log("Loading image failed: %s", stbi_failure_reason());
+        exit(1);
+    }
+
+    // Set up the pixel format color masks for RGB(A) byte arrays.
+    // Only STBI_rgb (3) and STBI_rgb_alpha (4) are supported here!
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    int shift = (req_format == STBI_rgb) ? 8 : 0;
+    const Uint32 rmask = 0xff000000 >> shift;
+    const Uint32 gmask = 0x00ff0000 >> shift;
+    const Uint32 bmask = 0x0000ff00 >> shift;
+    const Uint32 amask = 0x000000ff >> shift;
+#else // little endian, like x86
+    const Uint32 rmask = 0x000000ff;
+    const Uint32 gmask = 0x0000ff00;
+    const Uint32 bmask = 0x00ff0000;
+    const Uint32 amask = (req_format == STBI_rgb) ? 0 : 0xff000000;
+#endif
+
+    // STBI_rgb_alpha (RGBA)
+    int depth = 32;
+    int pitch = 4*width;
+
+    return sdl_check_pointer(SDL_CreateRGBSurfaceFrom((void*)pixels, width, height, depth, pitch,
+                                                      rmask, gmask, bmask, amask));
+}
+
 int main(int argc, char *argv[])
 {
     (void) argc;
@@ -27,9 +66,22 @@ int main(int argc, char *argv[])
     sdl_check_code(SDL_Init(SDL_INIT_VIDEO));
     SDL_Window *window = sdl_check_pointer(SDL_CreateWindow("Rogueban", 0, 0, 512, 512, SDL_WINDOW_RESIZABLE));
     SDL_Renderer *renderer = sdl_check_pointer(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+    SDL_Surface *font_surface = sdl_check_pointer( get_suface_from_file(FONT));
+    SDL_Texture *font_texture = sdl_check_pointer(SDL_CreateTextureFromSurface(renderer, font_surface));
+    SDL_Rect font_rect = (SDL_Rect) {
+        .x = 18 - 1,
+        .y = 0,
+        .w = 18,
+        .h = 25
+    };
+    SDL_Rect font_dest = (SDL_Rect) {
+        .x = 0,
+        .y = 0,
+        .w = 18 * 4,
+        .h = 25 * 4
+    };
 
     bool quit = false;
-
     while (!quit) {
         SDL_Event event = {0};
         while (SDL_PollEvent(&event)) {
@@ -42,6 +94,7 @@ int main(int argc, char *argv[])
 
             sdl_check_code(SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255));
             sdl_check_code(SDL_RenderClear(renderer));
+            sdl_check_code(SDL_RenderCopy(renderer, font_texture, &font_rect, &font_dest));
             SDL_RenderPresent(renderer);
         }
     }
